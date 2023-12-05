faf0831c191dbdd8a3af34375c1e2c67df64352b
HoshinoRei <81515470+HoshinoRei@users.noreply.github.com>
2023-12-05 14:47:04 +0800

    refactor(vitest): migrating testing tool from Jest to Vitest (#229)
    - rename the job from `jest` to `vitest` for consistency with the testing framework used in `Test` workflow
    - update the path for the test result file to `test-result/junit.xml` to match the new location in `Test` workflow
    - update the `test` script to use `vitest run --coverage` command for running tests with coverage in `package.json`
    - update the import statement for test functions to use "vitest" instead of "@jest/globals" in all test source files
    - update the snapshot file to use `Vitest Snapshot` instead of `Jest Snapshot` and update the snapshot names to match the new test names

--- test/unit/BarkMessageBuilder.test.ts
+++ test/unit/BarkMessageBuilder.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "@jest/globals"
+import { describe, expect, test } from "vitest"
 
 import BarkMessageBuilder from "../../src/lib/BarkMessageBuilder"
 import BarkMessageErrorType from "../../src/model/enumeration/BarkMessageErrorType"
